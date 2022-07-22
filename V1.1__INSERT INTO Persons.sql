d58db9af0b64f0fec920464b501d543c41457bb6
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-22 11:01:40 +0530

    Delete V1.1__INSERT INTO Persons.sql

--- V1.1__INSERT INTO Persons.sql
+++
@@ -1,5 +0,0 @@
-SELECT * INTO  backup.22-july-2022_public.Persons FROM public.Persons;
-
-INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES
-(007, 'ms', 'dhoni', 'india', 'ranchi'),
-(777, 'chris', 'gayle', 'westindies', 'dwain');
