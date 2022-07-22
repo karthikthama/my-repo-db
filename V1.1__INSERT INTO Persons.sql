9405438e141e924d64fe95afcd4c0554ad885824
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-22 10:56:34 +0530

    Update V1.1__INSERT INTO Persons.sql

--- V1.1__INSERT INTO Persons.sql
+++ V1.1__INSERT INTO Persons.sql
@@ -1,3 +1,5 @@
-SELECT * INTO  backup.22.july.2022_public.Persons FROM public.Persons;
+SELECT * INTO  backup.22-july-2022_public.Persons FROM public.Persons;
 
-INSERT INTO Persons 
+INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES
+(007, 'ms', 'dhoni', 'india', 'ranchi'),
+(777, 'chris', 'gayle', 'westindies', 'dwain');
