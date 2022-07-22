b5d6a7164e0b5e7ac871d455b7cccb4d3d0bd3f8
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-22 09:37:42 +0530

    Update V1__CREATE TABLE SAMPLE USER DETAILS.sql

--- V1__CREATE TABLE SAMPLE USER DETAILS.sql
+++ V1__CREATE TABLE SAMPLE USER DETAILS.sql
@@ -1,4 +1,4 @@
-CREATE TABLE  user_details (
+CREATE TABLE  user_details(
   user_id int(11) NOT NULL AUTO_INCREMENT,
   username varchar(255) DEFAULT NULL,
   first_name varchar(50) DEFAULT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE  user_details (
   password varchar(50) DEFAULT NULL,
   status tinyint(10) DEFAULT NULL,
   PRIMARY KEY (user_id)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
+);
 
 
 INSERT INTO user_details (user_id, username, first_name, last_name, gender, password, status) VALUES
