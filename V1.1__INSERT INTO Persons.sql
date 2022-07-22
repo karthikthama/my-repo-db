507c1e1507d4720564f75884881e923b4496918d
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-22 11:19:41 +0530

    Update V1.1__INSERT INTO Persons.sql

--- V1.1__INSERT INTO Persons.sql
+++ V1.1__INSERT INTO Persons.sql
@@ -1,4 +1,4 @@
-CREATE TABLE backup.bkp_tbls_1st.22-july-2022_public_Persons as select * FROM public.Persons;
+CREATE TABLE backup.bkp_tbls_1st_22_july_2022_public_Persons as select * FROM public.Persons;
 
 INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES
 (007, 'ms', 'dhoni', 'india', 'ranchi'),
