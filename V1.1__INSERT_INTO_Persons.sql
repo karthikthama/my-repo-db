6502a8d33f7b75a8618483d005a4bd6c649fc0f5
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-28 10:59:59 +0530

    Delete V1.1__INSERT_INTO_Persons.sql

--- V1.1__INSERT_INTO_Persons.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE backup.bkp_tbls_1st_22_july_2022_public_Persons as select * FROM public.Persons;
-
-INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES
-(007, 'ms', 'dhoni', 'india', 'ranchi'),
-(777, 'chris', 'gayle', 'westindies', 'dwain');
