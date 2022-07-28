6ac2eb8b38f8b4f943eb6c54b6688162fecf4dc4
hemanth <hemanthkatkozu@gmail.com>
2022-07-28 07:31:53 +0000

    BH984859365

---
+++ V4__CREATE_TABLE_Players.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Players (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+INSERT INTO Players (PersonID, LastName, FirstName, Address, City) VALUES
+(123, 'sam', 'billings', 'Australia', 'Melbourne'),
+(018, 'virat', 'kohli', 'Banglore', 'whitefield');
